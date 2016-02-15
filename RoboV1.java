a8a84903dba23c9b57ce5ee04df21ce0de03ca37
Renato Ely <renato.silva@ccc.ufcg.edu.br>
2016-02-15 20:15:56 -0300

    Versão 1 do robo

---
+++ RoboV1.java
@@ -0,0 +1,80 @@
+package ufcg;
+import robocode.*;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+//import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * RoboV1 - a robot by (your name here)
+ */
+public class RoboV1 extends AdvancedRobot
+{	
+	double gunTurnAmt;
+	/**
+	 * run: RoboV1's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			setAhead(100);
+			setTurnRight(360);
+			setTurnGunRight(360);
+			execute();
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		if (e.getDistance() > 150) {
+			gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+
+			turnGunRight(gunTurnAmt); // Try changing these to setTurnGunRight,
+			turnRight(e.getBearing()); // and see how much Tracker improves...
+			// (you'll have to make Tracker an AdvancedRobot)
+			ahead(e.getDistance() - 140);
+			return;
+		}
+
+		// Our target is close.
+		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+		turnGunRight(gunTurnAmt);
+		fire(3);
+
+		// Our target is too close!  Back up.
+		if (e.getDistance() < 100) {
+			if (e.getBearing() > -90 && e.getBearing() <= 90) {
+				back(40);
+			} else {
+				ahead(40);
+			}
+		}
+		scan();
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
